eb5a7e195f1bd4b0546ccc31e6d8cb97cb14d5ae
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-15 21:25:17 +0530

    Add Question 10 inside LearnBasics.Builduplogical

---
+++ src/LearnBasics/Builduplogical/Question10.java
@@ -0,0 +1,24 @@
+package LearnBasics.Builduplogical;
+
+import java.util.Scanner;
+
+public class Question10 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number : ");
+        int a = sc.nextInt();
+        for (int i = 0;i<2*a-1;i++){
+            if (i<a){
+                for (int j=0;j<=i;j++){
+                    System.out.print("* ");
+                }
+            }
+            else{
+                for (int j = 0;j<(2*a-i-1);j++){
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
