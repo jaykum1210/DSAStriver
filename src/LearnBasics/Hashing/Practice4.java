c7a9c908c1a5f4acb424363804bea83d044f7818
jaykum1210 <jaykumawat1210@gmail.com>
2026-03-17 20:06:12 +0530

    Add Practice 5 inside LearnBasics.Hashing

--- src/LearnBasics/Hashing/Practice4.java
+++ src/LearnBasics/Hashing/Practice4.java
@@ -5,18 +5,12 @@ import java.util.HashMap;
 public class Practice4 {
     public static void main(String[] args){
         String a = "Java is easy java is powerful";
-        // split sentence into words
         String[] words = a.split(" ");
-        // create hashmap
         HashMap<String, Integer> map = new HashMap<>();
-        // count frequency
         for(int i = 0; i < words.length; i++){
-
             String w = words[i].toLowerCase();
-
             map.put(w, map.getOrDefault(w, 0) + 1);
         }
-        // print result
         for(String key : map.keySet()){
             System.out.println(key + " -> " + map.get(key));
         }
