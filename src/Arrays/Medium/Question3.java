d292dec85b3430042568eb24ed4bd43e44b8a50e
jaykum1210 <jaykumawat1210@gmail.com>
2026-05-07 23:14:16 +0530

    Add Question 3 inside Array.Medium

---
+++ src/Arrays/Medium/Question3.java
@@ -0,0 +1,30 @@
+package Arrays.Medium;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Question3 {
+    public static int majorityElement(int[] arr){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int e : arr){
+            map.put(e,map.getOrDefault(e,0)+1);
+        }
+        for (int e : map.keySet()){
+            if (map.get(e)>=(arr.length/2)){
+                return e;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size : ");
+        int a = sc.nextInt();
+        int[] arr = new int[a];
+        System.out.println("Enter " + a + " Elements : ");
+        for (int i = 0;i<a;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Majority Number : " + majorityElement(arr));
+    }
+}
