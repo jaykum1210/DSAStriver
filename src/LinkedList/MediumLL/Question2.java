873978888ab91c2bf859f7f843aa65cf10b71e09
Jay Kumawat <jaykumawat1210@gmail.com>
2026-06-10 17:52:35 +0530

    Add Question1 to LinkedList MediumLL

--- src/LinkedList/MediumLL/Question2.java
+++ src/LinkedList/MediumLL/Question2.java
@@ -1,10 +1,10 @@
-package LinkedList.MediumLL;
-
-import java.util.Scanner;
-
-public class Question2 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
-    }
-}
+//package LinkedList.MediumLL;
+//
+//import java.util.Scanner;
+//
+//public class Question2 {
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//
+//    }
+//}
